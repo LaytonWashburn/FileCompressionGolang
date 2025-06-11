201b81d7447ffa59819bff750ad5304c412354b7
Layton Washburn <layton.washburn1659@gmail.com>
2025-06-10 22:18:23 -0600

    Added description to main.go

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+// Main entry point for the file compression cli tool
+
 package main
 
 import "fileCompression/cmd"
@@ -6,47 +8,3 @@ import "fileCompression/cmd"
 func main() {
 	cmd.Execute()
 }
-
-
-
-
-
-// func validateArguments() {
-
-
-
-// }
-
-// // checks for error
-// func ErrorChecker(err error)  {
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// }
-
-// func main() {
-
-// 	println("Welcome to the File Compression Project!")
-
-// 	// // Open the file
-// 	// file_name := "data.txt"
-// 	// file_path := "C:/Users/layto/Coding/Golang/FileCompression/data/"
-// 	// output_path := "C:/Users/layto/Coding/Golang/FileCompression/output/"
-
-// 	inputFile := flag.String("file", "", "Input file to compress")
-// 	// outputFile := flag.String("out", "", "Output file name (optional)")
-// 	flag.Parse()
-
-// 	if *inputFile == "" {
-// 		fmt.Println("Usage: go run main.go -in=input.txt [-out=output.huff]")
-// 		return
-// 	}
-
-// 	// fmt.Println("Compressing:", *inputFile)
-// 	// if *outputFile != "" {
-// 	// 	fmt.Println("Output will be saved to:", *outputFile)
-// 	// }
-
-
-
-// }
